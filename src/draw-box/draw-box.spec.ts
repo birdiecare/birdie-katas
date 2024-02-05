723908299c68cc8b2dadaaebb760c8e5f199828a
Greg Cann <46439815+greg-cann@users.noreply.github.com>
2024-02-05 14:56:32 +0000

    Update draw-box.spec.ts
    Don't run the tests by default

--- src/draw-box/draw-box.spec.ts
+++ src/draw-box/draw-box.spec.ts
@@ -1,22 +1,22 @@
 import {drawBox} from "./draw-box";
 
-describe('.drawBox', () => {
-  test("should draw a 2x2 box", () => {
-    expect(drawBox(2, 2)).toEqual(`
-+--+
-|  |
-|  |
-+--+`
-    )
-  });
+// describe('.drawBox', () => {
+//   test("should draw a 2x2 box", () => {
+//     /*expect(drawBox(2, 2)).toEqual(`
+// +--+
+// |  |
+// |  |
+// +--+`
+//     )
+//   });
 
-  test("should draw a 6x3 box", () => {
-    expect(drawBox(6, 3)).toEqual( `
-+------+
-|      |
-|      |
-|      |
-+------+`)
-  });
-});
+//   test("should draw a 6x3 box", () => {
+//     expect(drawBox(6, 3)).toEqual( `
+// +------+
+// |      |
+// |      |
+// |      |
+// +------+`)
+//   });
+// });
 
